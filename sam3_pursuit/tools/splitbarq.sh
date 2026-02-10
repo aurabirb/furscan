2f642da5edf50eb43ea7f77820f4a755a48c41ea
aurabirb <fed0000r@gmail.com>
2026-02-10 15:40:28 +0100

    Fix dataset source in splitbarq

--- sam3_pursuit/tools/splitbarq.sh
+++ sam3_pursuit/tools/splitbarq.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 for i in {1..9}; do
-    mkdir -p datasets/barq$i;
-    find barq_images -maxdepth 1 -name "$i*" -exec "mv {} datasets/barq$i/" \; ;
+    mkdir -p datasets/barq$i/barq;
+    find barq_images -maxdepth 1 -name "$i*" -exec "mv {} datasets/barq$i/barq/" \; ;
 done
 
 for i in {1..9}; do
     echo $i
-done | xargs -P 4 -I {} pursuit -ds barq{} ingest barq --data-dir datasets/barq{}
+done | xargs -P 4 -I {} pursuit -ds barq{} ingest barq --data-dir datasets/barq{}/barq/
